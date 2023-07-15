6548c5ec97d2b15c4fee65579f617415ef805566
ErliK1 <ekuka21@epoka.edu.al>
2023-07-15 14:03:02 +0200

    Problem

--- first_project/first_app/serializers.py
+++ first_project/first_app/serializers.py
@@ -40,4 +40,57 @@ class CourseSerializerUpdater(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = '__all__cd '
+        fields = '__all__'
+
+class ProfessorSerializerGet(serializers.ModelSerializer):
+
+    course =  CourseSerializerCreate()
+    class Meta:
+        model = Professor
+        fields = '__all__'
+
+class ProfessorSerializerCreate(serializers.ModelSerializer):
+
+    #course = CourseSerializerCreate
+    class Meta:
+        model = Professor
+        fields = '__all__'
+
+class ProfessorCourseSerializerCreate(serializers.ModelSerializer):
+
+    course = CourseSerializerGet()
+    class Meta:
+        model = Professor
+        fields = '__all__'
+
+    # def create(self):
+
+class TeacherSerializer(serializers.ModelSerializer):
+    #student = StudentSerializer()
+
+
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+class TeacherStudentSerializer(serializers.ModelSerializer):
+
+    student = StudentSerializer()
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+    # def create(self, validated_data):
+    #     student_detail = validated_data.pop('student')
+    #     self.student = StudentSerializer(data=student_detail)
+    #     if(self.student.is_valid()):
+    #         self.student.save()
+    #
+    #
+
+
+
+
+
+
+
